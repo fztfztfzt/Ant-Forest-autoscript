9f33da4b998dcd908f2262759f7d17d2a1be97ff
fangzhengtao <fztfztfzt@126.com>
2019-10-20 12:16:45 +0800

    有道自动签到完善，增加退出

--- YoudaoNode.js
+++ YoudaoNode.js
@@ -2,12 +2,17 @@ auto();
 var Unlock = require("./lib/Unlock.js")
 Unlock().exec()
 launchApp("有道云笔记");
-sleep(5000)
-click(984,2255)
+id("tab_name").className("android.widget.TextView").text("我的").waitFor()
 sleep(1000)
-click(932,316)
-sleep(1000)
-id("btn_ok").findOne().click()
-sleep(1000)
+id("tab_mine").findOne().click()
+sleep(500)
+id("sign_in").findOne().click()
+sleep(500)
 log(id("sign_message").findOne().text())
+sleep(500)
 log('youdaoNode success')
+id("btn_ok").waitFor()
+sleep(500)
+id("btn_ok").findOne().click()
+back();
+back();
